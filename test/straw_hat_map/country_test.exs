5a2137c1fddca70c384203b39c0c704a898b8d68
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2017-09-05 05:09:02 -0400

    cleaning up test

--- test/straw_hat_map/country_test.exs
+++ test/straw_hat_map/country_test.exs
@@ -4,6 +4,7 @@ defmodule StrawHatMapTest.CountryTest do
 
   test "get country by id" do
     country = insert(:country)
+
     assert {:ok, _country} = Country.find_country(country.id)
   end
 
@@ -11,20 +12,19 @@ defmodule StrawHatMapTest.CountryTest do
     assert {:error, _reason} = Country.find_country(8745)
   end
 
-  test "list countries with pagination" do
+  test "list of countries" do
     insert_list(10, :country)
     country = Country.country_list(%{page: 2, page_size: 5})
+
     assert country.total_entries == 10
   end
 
-
-  test "list countries by ids" do
-    available_countries = insert_list(10, :country)
+  test "list of countries by ids" do
+    available_countries = insert_list(3, :country)
     ids =
       available_countries
       |> Enum.take(2)
       |> Enum.map(fn country -> country.id end)
-
     countries = Country.get_countries_by_ids(ids)
 
     assert List.first(countries).id == List.first(ids)
@@ -46,11 +46,13 @@ defmodule StrawHatMapTest.CountryTest do
   test "update by country" do
     country = insert(:country)
     {:ok, country} = Country.update_country(country, %{"name": "Cuba"})
+
     assert country.name == "Cuba"
   end
 
   test "delete country" do
     country = insert(:country)
+
     assert {:ok, _} = Country.destroy_country(country)
   end
 end

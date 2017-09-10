b70b4af22ffd817999f867c8c928590b3f9aafd3
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2017-09-10 02:27:11 -0500

    fix merging

--- lib/straw_hat_map/schema/place.ex
+++ lib/straw_hat_map/schema/place.ex
@@ -10,14 +10,9 @@ defmodule StrawHat.Map.Schema.Place do
   schema "places" do
     field(:name, :string)
     field(:active, :boolean, default: true)
-<<<<<<< HEAD
-    field(:owner_id, :integer)
-    belongs_to(:address, Address)
-=======
     field(:owner_id, :string)
     belongs_to(:place, Place)
     belongs_to(:location, Location)
->>>>>>> develop
   end
 
   def changeset(place, place_attrs) do

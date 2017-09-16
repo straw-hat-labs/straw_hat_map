702f40ae0a91634ee706f2241267c412788cc6eb
Osley Zorrilla Rivera <ozorrilla87@gmail.com>
2017-09-16 00:13:54 -0700

    remove unused functions

--- lib/straw_hat_map/location.ex
+++ lib/straw_hat_map/location.ex
@@ -4,19 +4,6 @@ defmodule StrawHat.Map.Location do
   alias StrawHat.Map.Query.LocationQuery
   alias StrawHat.Map.Schema.Location
 
-  def destroy_location(%Location{} = location), do: Repo.delete(location)
-  
-  def find_location(location_id) do
-    case get_location(location_id) do
-      nil ->
-        error = Error.new("map.location.not_found", metadata: [location_id: location_id])
-        {:error, error}
-      location -> {:ok, location}
-    end
-  end
-
-  def get_location(location_id), do: Repo.get(Location, location_id)
-
   def get_locations_by_ids(location_ids) do
     Location
     |> LocationQuery.by_ids(location_ids)

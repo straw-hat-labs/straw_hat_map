8b9115d102b97192f0811701a303186a0439f438
Osley Zorrilla Rivera <ozorrilla87@gmail.com>
2017-09-16 00:14:56 -0700

    add location test

---
+++ test/straw_hat_map/location_test.exs
@@ -0,0 +1,16 @@
+defmodule StrawHatMapTest.LocationTest do
+  use StrawHatMapTest.DataCase, async: true
+  alias StrawHat.Map.Location
+
+  test "get locations by ids" do
+    available_locations = insert_list(3, :location)
+    ids =
+      available_locations
+      |> Enum.take(2)
+      |> Enum.map(fn location -> location.id end)
+    locations = Location.get_locations_by_ids(ids)
+
+    assert List.first(locations).id == List.first(ids)
+    assert List.last(locations).id == List.last(ids)
+  end
+end

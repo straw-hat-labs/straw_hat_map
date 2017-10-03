9f78694b980e4c956b0fa88fb8935ab3d291ff7c
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2017-10-03 01:46:23 -0400

    updating packages

--- mix.exs
+++ mix.exs
@@ -1,46 +1,42 @@
 defmodule StrawHat.Map.Mixfile do
   use Mix.Project
 
-  @version "0.2.2"
-
-  @elixir_version "~> 1.5"
-  @name :straw_hat_map
   @organization "straw_hat"
+  @name :straw_hat_map
+  @version "0.2.2"
+  @elixir_version "~> 1.5"
+
   @description """
-    Addresses Management
+    Addresses Management.
   """
-  @source_url "https://github.com/straw-hat-llc/straw_hat_map"
+  @source_url "https://github.com/straw-hat-team/straw_hat_map"
 
   def project do
     production? = Mix.env == :prod
 
     [
-      app: @name,
+      name: "StrawHat.Map",
       description: @description,
+
+      app: @name,
       version: @version,
       elixir: @elixir_version,
       elixirc_paths: elixirc_paths(Mix.env),
-      build_embedded: production?,
-      start_permanent: production?,
       deps: deps(),
       aliases: aliases(),
-      package: package(),
 
-      # docs
-      name: "StrawHat.Map",
-      source_url: @source_url,
-      homepage_url: @source_url,
-      docs: [
-        main: "StrawHat.Map",
-        extras: ["README.md"]
-      ],
+      build_embedded: production?,
+      start_permanent: production?,
 
-      # coverage
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
         "coveralls": :test,
         "coveralls.html": :test
-      ]
+      ],
+
+      # Extras
+      package: package(),
+      docs: docs()
     ]
   end
 
@@ -54,20 +50,21 @@ defmodule StrawHat.Map.Mixfile do
 
   defp deps do
     [
-      {:straw_hat, "~> 0.0.9", organization: "straw_hat"},
+      {:straw_hat, "~> 0.1.2"},
 
       {:postgrex, "~> 0.13.2"},
       {:ecto, "~> 2.2"},
       {:scrivener_ecto, "~> 1.2"},
 
       # Testing
-      {:ex_machina, ">= 0.0.0", only: :test},
-      {:faker, ">= 0.0.0", only: :test},
+      {:ex_machina, ">= 0.0.0", only: [:test]},
+      {:faker, ">= 0.0.0", only: [:test]},
 
       # Tools
-      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:dialyxir, ">= 0.0.0", only: [:dev], runtime: false},
       {:credo, ">= 0.0.0", only: [:dev, :test], runtime: false},
-      {:excoveralls, ">= 0.0.0", only: :test}
+      {:excoveralls, ">= 0.0.0", only: [:test], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: [:dev], runtime: false}
     ]
   end
 
@@ -91,6 +88,16 @@ defmodule StrawHat.Map.Mixfile do
       maintainers: ["Yordis Prieto"],
       licenses: ["MIT"],
       links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      homepage_url: @source_url,
+      source_ref: "v#{@version}",
+      source_url: @source_url,
+      extras: ["README.md"]
     ]
   end
 end

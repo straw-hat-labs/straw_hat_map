9504a672ebce27ceda8ec536c6471ee8aa113523
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2018-03-03 15:00:25 -0800

    fix doc of location

--- lib/straw_hat_map/location.ex
+++ lib/straw_hat_map/location.ex
@@ -1,10 +1,17 @@
 defmodule StrawHat.Map.Location do
-  @moduledoc false
+  @moduledoc """
+  Defines functionality for locations.
+  """
+
   use StrawHat.Map.Interactor
 
   alias StrawHat.Map.Query.LocationQuery
   alias StrawHat.Map.Schema.Location
 
+  @doc """
+  Get list of locations.
+  """
+  @spec get_locations_by_ids([integer()]) :: [Location.t()] | no_return()
   def get_locations_by_ids(location_ids) do
     Location
     |> LocationQuery.by_ids(location_ids)
--- lib/straw_hat_map/schema/location.ex
+++ lib/straw_hat_map/schema/location.ex
@@ -1,8 +1,37 @@
 defmodule StrawHat.Map.Schema.Location do
-  @moduledoc false
+  @moduledoc """
+  Represents a Location Ecto Schema with functionality about the data validation
+  for Location.
+  """
 
   use StrawHat.Map.Schema
   alias StrawHat.Map.Schema.{Address, Place}
+
+  @typedoc """
+  - `longitude`: Longitude of the location.
+  - `latitude`: Latitude of the location.
+  - `address`: `t:StrawHat.Map.Schema.Address.t/0` associated with the location.
+  - `address_id`: `id` of `t:StrawHat.Map.Schema.Address.t/0` associated with
+  the location.
+  - `place`: `t:StrawHat.Map.Schema.Place.t/0` associated with the location.
+  """
+  @type t :: %__MODULE__{
+          longitude: float(),
+          latitude: float(),
+          address_id: String.t(),
+          address: Address.t() | Ecto.Association.NotLoaded.t(),
+          place: Place.t() | Ecto.Association.NotLoaded.t()
+        }
+
+  @typedoc """
+  Check `t:t/0` type for more information about the keys.
+  """
+  @type location_attrs :: %{
+          longitude: float(),
+          latitude: float(),
+          address_id: String.t()
+        }
+
 
   @optional_fields ~w(longitude latitude address_id)a
 
@@ -13,6 +42,10 @@ defmodule StrawHat.Map.Schema.Location do
     has_one(:place, Place)
   end
 
+  @doc """
+  Validate the attributes and return a Ecto.Changeset for the current Location.
+  """
+  @spec changeset(t, location_attrs) :: Ecto.Changeset.t()
   def changeset(location, location_attrs) do
     location
     |> cast(location_attrs, @optional_fields)

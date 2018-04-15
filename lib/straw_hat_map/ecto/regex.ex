2267d93aa510ab4092281382035a45607a62d90d
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2018-04-15 02:28:48 -0400

    fixing regex ecto type

--- lib/straw_hat_map/ecto/regex.ex
+++ lib/straw_hat_map/ecto/regex.ex
@@ -1,44 +1,34 @@
 defmodule StrawHat.Map.Ecto.Regex do
   @moduledoc """
-  Adds regular expressions as a custom Ecto Type
+  Adds regular expressions as a custom Ecto Type.
   """
 
   @behaviour Ecto.Type
+
   def type, do: :string
 
-  @doc """
-  Cast regular expressions into strings
-  """
-  @spec cast(Regex.t()) :: {:ok, String.t()} | :error
+  @since "1.1.0"
+  @spec cast(String.t) :: {:ok, String.t()} | :error
   def cast(value) when is_binary(value) do
-    value
-    |> Regex.compile()
-    |> with_valid_regex(value)
-  end
-
-  def cast(value) do
-    if Regex.regex?(value) do
-      {:ok, Regex.source(value)}
-    else
-      :error
+    case Regex.compile(value) do
+      {:ok, _} -> {:ok, value}
+      _ -> :error
     end
   end
 
-  @doc """
-  Loads the stored data as a regular expression
-  """
+  def cast(_), do: :error
+
+  @since "1.1.0"
   @spec load(String.t()) :: {:ok, Regex.t()} | :error
   def load(regex) when is_binary(regex) do
     Regex.compile(regex)
   end
 
+  @since "1.1.0"
   def load(_), do: :error
 
+  @since "1.1.0"
   @spec dump(String.t()) :: {:ok, String.t()} | :error
   def dump(regex) when is_binary(regex), do: {:ok, regex}
   def dump(_), do: :error
-
-  @spec with_valid_regex({:ok, Regex.t()} | {:error, any()}, String.t()) :: {:ok, String.t()} | :error
-  defp with_valid_regex({:ok, _}, value), do: {:ok, value}
-  defp with_valid_regex(_, _), do: :error
 end

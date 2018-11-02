ba92edd03054de3ef278988c6a0b4b8b247be4b1
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2018-11-02 02:02:47 -0700

    Fix warning

--- lib/straw_hat_map/repo.ex
+++ lib/straw_hat_map/repo.ex
@@ -3,6 +3,9 @@ defmodule StrawHat.Map.Repo do
   Defines a repository.
   Check `Ecto.Repo` documentation for learn more about this module.
   """
-  use Ecto.Repo, otp_app: :straw_hat_map
+  use Ecto.Repo,
+    otp_app: :straw_hat_map,
+    adapter: Ecto.Adapters.Postgres
+
   use Scrivener, page_size: 25
 end

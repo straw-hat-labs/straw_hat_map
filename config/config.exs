8b326367f285ada5e5113bc7fff5152e9adbfa4b
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2018-11-24 16:44:05 -0800

    Remove repo adapter from config

--- config/config.exs
+++ config/config.exs
@@ -3,7 +3,6 @@ use Mix.Config
 config :straw_hat_map, ecto_repos: [StrawHat.Map.Repo]
 
 config :straw_hat_map, StrawHat.Map.Repo,
-  adapter: Ecto.Adapters.Postgres,
   database: "straw_hat_map_test",
   username: "postgres",
   hostname: "localhost",

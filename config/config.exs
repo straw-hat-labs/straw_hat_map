a8a4e5f851d67b2ee1fa82d7fd4b9050ae0fe711
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-01-13 00:44:44 -0800

    Fix username

--- config/config.exs
+++ config/config.exs
@@ -4,6 +4,7 @@ config :straw_hat_map, ecto_repos: [StrawHat.Map.Repo]
 
 config :straw_hat_map, StrawHat.Map.Repo,
   database: "straw_hat_map",
+  username: "postgres",
   pool: Ecto.Adapters.SQL.Sandbox,
   types: StrawHat.Map.Ecto.AdapterTypes
 

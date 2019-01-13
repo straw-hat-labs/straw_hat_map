4d440cb18cae855f5ff2828dc5e50e56a0eec2c0
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-01-13 02:14:44 -0800

    Removes docs

--- lib/straw_hat_map/world/countries/countries.ex
+++ lib/straw_hat_map/world/countries/countries.ex
@@ -7,17 +7,11 @@ defmodule StrawHat.Map.Countries do
   alias StrawHat.{Error, Response}
   alias StrawHat.Map.{Country, State}
 
-  @doc """
-  Gets the list of countries.
-  """
   @spec get_countries(Ecto.Repo, Scrivener.Config.t()) :: Scrivener.Page.t()
   def get_countries(repo, pagination \\ []) do
     repo.paginate(Country, pagination)
   end
 
-  @doc """
-  Creates a country.
-  """
   @spec create_country(Ecto.Repo, Country.country_attrs()) :: Response.t(Country.t(), Ecto.Changeset.t())
   def create_country(repo, country_attrs) do
     %Country{}
@@ -26,9 +20,6 @@ defmodule StrawHat.Map.Countries do
     |> Response.from_value()
   end
 
-  @doc """
-  Updates a country.
-  """
   @spec update_country(Ecto.Repo, Country.t(), Country.country_attrs()) ::
           Response.t(Country.t(), Ecto.Changeset.t())
   def update_country(repo, %Country{} = country, country_attrs) do
@@ -38,9 +29,6 @@ defmodule StrawHat.Map.Countries do
     |> Response.from_value()
   end
 
-  @doc """
-  Destroys a country.
-  """
   @spec destroy_country(Ecto.Repo, Country.t()) :: Response.t(Country.t(), Ecto.Changeset.t())
   def destroy_country(repo, %Country{} = country) do
     country
@@ -48,9 +36,6 @@ defmodule StrawHat.Map.Countries do
     |> Response.from_value()
   end
 
-  @doc """
-  Gets a country by `id`.
-  """
   @spec find_country(Ecto.Repo, String.t()) :: Response.t(Country.t(), Error.t())
   def find_country(repo, country_id) do
     repo
@@ -60,26 +45,17 @@ defmodule StrawHat.Map.Countries do
     )
   end
 
-  @doc """
-  Gets a country by `id`.
-  """
   @spec get_country(Ecto.Repo, String.t()) :: Country.t() | nil | no_return
   def get_country(repo, country_id) do
     repo.get(Country, country_id)
   end
 
-  @doc """
-  Gets list of countries.
-  """
   @spec get_countries_by_ids(Ecto.Repo, [integer()]) :: [Country.t()] | no_return()
   def get_countries_by_ids(repo, country_ids) do
     query = from(country in Country, where: country.id in ^country_ids)
     repo.all(query)
   end
 
-  @doc """
-  Gets list of states.
-  """
   @spec get_states(Ecto.Repo, [integer()]) :: [State.t()] | no_return()
   def get_states(repo, country_ids) when is_list(country_ids) do
     query = from(state in State, where: state.country_id in ^country_ids)

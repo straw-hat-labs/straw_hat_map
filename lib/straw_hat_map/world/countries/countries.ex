a3dbda860ddeb7b62426ddf8832328e7b892add6
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-01-13 02:14:56 -0800

    Run formatter

--- lib/straw_hat_map/world/countries/countries.ex
+++ lib/straw_hat_map/world/countries/countries.ex
@@ -12,7 +12,8 @@ defmodule StrawHat.Map.Countries do
     repo.paginate(Country, pagination)
   end
 
-  @spec create_country(Ecto.Repo, Country.country_attrs()) :: Response.t(Country.t(), Ecto.Changeset.t())
+  @spec create_country(Ecto.Repo, Country.country_attrs()) ::
+          Response.t(Country.t(), Ecto.Changeset.t())
   def create_country(repo, country_attrs) do
     %Country{}
     |> Country.changeset(country_attrs)

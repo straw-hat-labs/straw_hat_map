1118966d730e4ade587dc6499b5500056fe79323
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-01-14 02:53:10 -0800

    Fix state use cases

--- lib/straw_hat_map/world/states/states.ex
+++ lib/straw_hat_map/world/states/states.ex
@@ -5,72 +5,71 @@ defmodule StrawHat.Map.States do
 
   import Ecto.Query, only: [from: 2]
   alias StrawHat.{Error, Response}
-  alias StrawHat.Map.Repo
   alias StrawHat.Map.{State, County, City}
 
-  @spec get_states(Scrivener.Config.t()) :: Scrivener.Page.t()
-  def get_states(pagination \\ []) do
-    Repo.paginate(State, pagination)
+  @spec get_states(Ecto.Repo.t(), Scrivener.Config.t()) :: Scrivener.Page.t()
+  def get_states(repo, pagination \\ []) do
+    repo.paginate(State, pagination)
   end
 
-  @spec create_state(State.state_attrs()) :: Response.t(State.t(), Ecto.Changeset.t())
-  def create_state(state_attrs) do
+  @spec create_state(Ecto.Repo.t(), State.state_attrs()) :: Response.t(State.t(), Ecto.Changeset.t())
+  def create_state(repo, state_attrs) do
     %State{}
     |> State.changeset(state_attrs)
-    |> Repo.insert()
+    |> repo.insert()
     |> Response.from_value()
   end
 
-  @spec update_state(State.t(), State.state_attrs()) :: Response.t(State.t(), Ecto.Changeset.t())
-  def update_state(%State{} = state, state_attrs) do
+  @spec update_state(Ecto.Repo.t(), State.t(), State.state_attrs()) :: Response.t(State.t(), Ecto.Changeset.t())
+  def update_state(repo, %State{} = state, state_attrs) do
     state
     |> State.changeset(state_attrs)
-    |> Repo.update()
+    |> repo.update()
     |> Response.from_value()
   end
 
-  @spec destroy_state(State.t()) :: Response.t(State.t(), Ecto.Changeset.t())
-  def destroy_state(%State{} = state) do
+  @spec destroy_state(Ecto.Repo.t(), State.t()) :: Response.t(State.t(), Ecto.Changeset.t())
+  def destroy_state(repo, %State{} = state) do
     state
-    |> Repo.delete()
+    |> repo.delete()
     |> Response.from_value()
   end
 
-  @spec find_state(String.t()) :: Response.t(State.t(), Error.t())
-  def find_state(state_id) do
-    state_id
-    |> get_state()
+  @spec find_state(Ecto.Repo.t(), String.t()) :: Response.t(State.t(), Error.t())
+  def find_state(repo, state_id) do
+    repo
+    |> get_state(state_id)
     |> Response.from_value(
       Error.new("straw_hat_map.state.not_found", metadata: [state_id: state_id])
     )
   end
 
-  @spec get_state(String.t()) :: State.t() | nil | no_return
-  def get_state(state_id) do
-    Repo.get(State, state_id)
+  @spec get_state(Ecto.Repo.t(), String.t()) :: State.t() | nil | no_return
+  def get_state(repo, state_id) do
+    repo.get(State, state_id)
   end
 
-  @spec get_states_by_ids([integer()]) :: [State.t()] | no_return()
-  def get_states_by_ids(state_ids) do
+  @spec get_states_by_ids(Ecto.Repo.t(), [integer()]) :: [State.t()] | no_return()
+  def get_states_by_ids(repo, state_ids) do
     query = from(state in State, where: state.id in ^state_ids)
-    Repo.all(query)
+    repo.all(query)
   end
 
-  @spec get_cities(State.t()) :: [City.t()] | no_return()
-  def get_cities(%State{} = state) do
+  @spec get_cities(Ecto.Repo.t(), State.t()) :: [City.t()] | no_return()
+  def get_cities(repo, %State{} = state) do
     query = from(city in City, where: city.state_id == ^state.id)
-    Repo.all(query)
+    repo.all(query)
   end
 
-  @spec get_cities([integer()]) :: [City.t()] | no_return()
-  def get_cities(state_ids) when is_list(state_ids) do
+  @spec get_cities(Ecto.Repo.t(), [integer()]) :: [City.t()] | no_return()
+  def get_cities(repo, state_ids) when is_list(state_ids) do
     query = from(city in City, where: city.state_id in ^state_ids)
-    Repo.all(query)
+    repo.all(query)
   end
 
-  @spec get_counties([integer()]) :: [County.t()] | no_return()
-  def get_counties(state_ids) when is_list(state_ids) do
+  @spec get_counties(Ecto.Repo.t(), [integer()]) :: [County.t()] | no_return()
+  def get_counties(repo, state_ids) when is_list(state_ids) do
     query = from(county in County, where: county.state_id in ^state_ids)
-    Repo.all(query)
+    repo.all(query)
   end
 end
--- test/straw_hat_map/states_test.exs
+++ test/straw_hat_map/states_test.exs
@@ -1,22 +1,24 @@
 defmodule StrawHat.Map.StatesTest do
   use StrawHat.Map.Tests.DataCase, async: true
   alias StrawHat.Map.States
+  alias StrawHat.Map.Repo
 
   describe "find_state/1" do
     test "with valid id should returns the found state" do
       state = insert(:state)
 
-      assert {:ok, _state} = States.find_state(state.id)
+      assert {:ok, _state} = States.find_state(Repo, state.id)
     end
 
     test "with invalid id shouldn't return any state" do
-      assert {:error, _reason} = Ecto.UUID.generate() |> States.find_state()
+      state_id = Ecto.UUID.generate()
+      assert {:error, _reason} = States.find_state(Repo, state_id)
     end
   end
 
   test "get_states/1 returns a pagination of states" do
     insert_list(10, :state)
-    state_page = States.get_states(%{page: 2, page_size: 5})
+    state_page = States.get_states(Repo, %{page: 2, page_size: 5})
 
     assert state_page.total_entries == 10
   end
@@ -25,13 +27,13 @@ defmodule StrawHat.Map.StatesTest do
     country = insert(:country)
     params = params_for(:state, %{country_id: country.id})
 
-    assert {:ok, _state} = States.create_state(params)
-    assert {:error, _state} = States.create_state(params)
+    assert {:ok, _state} = States.create_state(Repo, params)
+    assert {:error, _state} = States.create_state(Repo, params)
   end
 
   test "update_state/2 with valid inputs updates the state" do
     state = insert(:state)
-    {:ok, state} = States.update_state(state, %{name: "Havana"})
+    {:ok, state} = States.update_state(Repo, state, %{name: "Havana"})
 
     assert state.name == "Havana"
   end
@@ -39,7 +41,7 @@ defmodule StrawHat.Map.StatesTest do
   test "destroy_state/1 with a found state destroys the state" do
     state = insert(:state)
 
-    assert {:ok, _} = States.destroy_state(state)
+    assert {:ok, _} = States.destroy_state(Repo, state)
   end
 
   test "get_states_by_ids/1 with a list of IDs returns the relative states" do
@@ -50,7 +52,7 @@ defmodule StrawHat.Map.StatesTest do
       |> Enum.take(2)
       |> Enum.map(fn state -> state.id end)
 
-    states = States.get_states_by_ids(ids)
+    states = States.get_states_by_ids(Repo, ids)
 
     assert List.first(states).id == List.first(ids)
     assert List.last(states).id == List.last(ids)
@@ -59,7 +61,7 @@ defmodule StrawHat.Map.StatesTest do
   test "get_cities/1 returns the list of cities from the state" do
     state = insert(:state)
     insert_list(3, :city, %{state: state})
-    cities = States.get_cities(state)
+    cities = States.get_cities(Repo, state)
 
     assert length(cities) == 3
   end
@@ -71,7 +73,7 @@ defmodule StrawHat.Map.StatesTest do
     insert_list(2, :city, %{state: List.last(states)})
 
     ids = Enum.map(states, fn state -> state.id end)
-    cities = States.get_cities(ids)
+    cities = States.get_cities(Repo, ids)
 
     assert length(cities) == 4
   end
@@ -83,7 +85,7 @@ defmodule StrawHat.Map.StatesTest do
     insert_list(2, :county, %{state: List.last(states)})
 
     ids = Enum.map(states, fn state -> state.id end)
-    counties = States.get_counties(ids)
+    counties = States.get_counties(Repo, ids)
 
     assert length(counties) == 4
   end

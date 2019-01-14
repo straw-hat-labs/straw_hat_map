6e8406845658b6ffe83736f570965ce57caaa3d6
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-01-14 04:40:47 -0800

    Improve test messages

--- test/straw_hat_map/continents_test.exs
+++ test/straw_hat_map/continents_test.exs
@@ -2,19 +2,17 @@ defmodule StrawHat.Map.ContinentsTest do
   use StrawHat.Map.Tests.DataCase, async: true
   alias StrawHat.Map.Continents
 
-  test "get_continent_codes/0 fetch the list of continent codes" do
+  test "getting a list of continent's codes" do
     codes = Continents.get_continent_codes()
-
     assert codes == ["AF", "AN", "AS", "EU", "NA", "OC", "SA"]
   end
 
-  test "get_continents/0 fetch the list of continents" do
+  test "getting the list of continents" do
     continents = Continents.get_continents()
-
     assert length(continents) == 7
   end
 
-  test "find_continent/1 find a continent by code" do
+  test "finding a continent by code" do
     assert {:ok, _continent} = Continents.find_continent("AF")
     assert {:error, _continent} = Continents.find_continent("ASDF")
   end

af37d6703fe1e643a67c38868bad1cf36de57759
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-01-15 22:33:07 -0800

    Remove seed

--- mix.exs
+++ mix.exs
@@ -66,7 +66,7 @@ defmodule StrawHat.Map.Mixfile do
 
   defp aliases do
     [
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "ecto.migrate"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate", "test --trace"]
     ]

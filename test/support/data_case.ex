c976dee853cd11fd05d97b62dca7e53ae968ac62
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-01-19 06:23:32 -0800

    Fix aliases

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -2,24 +2,24 @@ defmodule StrawHat.Map.Tests.DataCase do
   @moduledoc false
 
   use ExUnit.CaseTemplate
+  alias Ecto.Adapters.SQL.Sandbox
+  alias StrawHat.Map.Repo
 
   using do
     quote do
       import Ecto
       import Ecto.Query
-
       import StrawHat.Map.Tests.DataCase
       import StrawHat.Map.Tests.Factory
-
       alias StrawHat.Map.Repo
     end
   end
 
   setup tags do
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(StrawHat.Map.Repo)
+    :ok = Sandbox.checkout(Repo)
 
     unless tags[:async] do
-      Ecto.Adapters.SQL.Sandbox.mode(StrawHat.Map.Repo, {:shared, self()})
+      Sandbox.mode(Repo, {:shared, self()})
     end
 
     :ok

c0be2dbc6fed80a147095500341c6b1c9a05b1d0
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-01-19 09:18:28 -0800

    Improve running all migrations

--- priv/repo/migrations/20170517232248_run_all_migrations.exs
+++ priv/repo/migrations/20170517232248_run_all_migrations.exs
@@ -14,14 +14,19 @@ defmodule StrawHat.Map.Repo.Migrations.RunAllMigrations do
   }
 
   def change do
-    CreateCountriesTable.change()
-    CreateStatesTable.change()
-    CreateCountiesTable.change()
-    CreateCitiesTable.change()
-    CreateAddressesTable.change()
-    CreateLocationsTable.change()
-    AddPostgisPlugin.change()
-    AddGeoLocationToLocationsTable.change()
-    AddPostalCodeRuleToCountries.change()
+    Enum.map(
+      [
+        CreateCountriesTable,
+        CreateStatesTable,
+        CreateCountiesTable,
+        CreateCitiesTable,
+        CreateAddressesTable,
+        CreateLocationsTable,
+        AddPostgisPlugin,
+        AddGeoLocationToLocationsTable,
+        AddPostalCodeRuleToCountries
+      ],
+      &apply(&1, :change, [])
+    )
   end
 end

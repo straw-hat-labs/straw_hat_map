4dfa59d237964986b6b12686d0a43895035423d2
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-01-20 02:20:30 -0800

    Improve state test cases

--- test/straw_hat_map/states_test.exs
+++ test/straw_hat_map/states_test.exs
@@ -44,26 +44,34 @@ defmodule StrawHat.Map.StatesTests do
     assert {:ok, _} = States.destroy_state(Repo, state)
   end
 
-  test "get_states_by_ids/1 with a list of IDs returns the relative states" do
-    available_states = insert_list(3, :state)
+  test "getting a list of states with a list of state's IDs" do
+    states_ids =
+      3
+      |> insert_list(:state)
+      |> Enum.map(&Map.get(&1, :id))
 
-    ids =
-      available_states
-      |> Enum.take(2)
-      |> Enum.map(fn state -> state.id end)
+    found_states_ids =
+      Repo
+      |> States.get_states_by_ids(states_ids)
+      |> Enum.map(&Map.get(&1, :id))
 
-    states = States.get_states_by_ids(Repo, ids)
-
-    assert List.first(states).id == List.first(ids)
-    assert List.last(states).id == List.last(ids)
+    assert states_ids == found_states_ids
   end
 
-  test "get_cities/1 returns the list of cities from the state" do
+  test "getting a list of cities of a state" do
     state = insert(:state)
-    insert_list(3, :city, %{state: state})
-    cities = States.get_cities(Repo, state)
 
-    assert length(cities) == 3
+    cities_ids =
+      3
+      |> insert_list(:city, %{state: state})
+      |> Enum.map(&Map.get(&1, :id))
+
+    found_cities_ids =
+      Repo
+      |> States.get_cities(state)
+      |> Enum.map(&Map.get(&1, :id))
+
+    assert cities_ids == found_cities_ids
   end
 
   test "get_cities/1 returns the list of cities from a list of state IDs" do

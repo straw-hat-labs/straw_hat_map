16068bbb6b3b647e532eab09dd7e17d15c877f6c
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-01-22 00:19:41 -0800

    Fix cities test cases

--- test/straw_hat_map/cities_test.exs
+++ test/straw_hat_map/cities_test.exs
@@ -2,56 +2,57 @@ defmodule StrawHat.Map.CitiesTests do
   use StrawHat.Map.TestSupport.CaseTemplate, async: true
   alias StrawHat.Map.Cities
 
-  describe "find_city/1" do
-    test "with valid id should find the city" do
+  describe "finding a city" do
+    test "with a valid ID" do
       city = insert(:city)
 
       assert {:ok, _city} = Cities.find_city(Repo, city.id)
     end
 
-    test "with invalid id shouldn't find the city" do
+    test "with an invalid ID" do
       city_id = Ecto.UUID.generate()
+
       assert {:error, _reason} = Cities.find_city(Repo, city_id)
     end
   end
 
-  test "get_cities/1 returns a pagination of cities" do
-    insert_list(5, :city)
-    city_page = Cities.get_cities(Repo, %{page: 2, page_size: 2})
+  test "returning a pagination of cities" do
+    insert_list(6, :city)
+    city_page = Cities.get_cities(Repo, %{page: 2, page_size: 5})
 
-    assert length(city_page.entries) == 2
+    assert length(city_page.entries) == 1
   end
 
-  test "create_city/1 with valid inputs creates a city" do
+  test "creating a city with valid inputs" do
     params = params_with_assocs(:city)
 
     assert {:ok, _city} = Cities.create_city(Repo, params)
   end
 
-  test "update_city/2 with valid inputs updates the city" do
+  test "updating a city with valid inputs" do
     city = insert(:city)
     {:ok, city} = Cities.update_city(Repo, city, %{name: "Havana"})
 
     assert city.name == "Havana"
   end
 
-  test "destroy_city/1 with a found city destroys the city" do
+  test "destroying an existing city" do
     city = insert(:city)
 
     assert {:ok, _} = Cities.destroy_city(Repo, city)
   end
 
-  test "get_cities_by_ids/1 with a list of IDs returns the relative cities" do
-    available_cities = insert_list(3, :city)
+  test "getting a list of cities with a list of city's IDs" do
+    cities_ids =
+      3
+      |> insert_list(:city)
+      |> Enum.map(&Map.get(&1, :id))
 
-    ids =
-      available_cities
-      |> Enum.take(2)
-      |> Enum.map(fn city -> city.id end)
+    found_cities_ids =
+      Repo
+      |> Cities.get_cities_by_ids(cities_ids)
+      |> Enum.map(&Map.get(&1, :id))
 
-    cities = Cities.get_cities_by_ids(Repo, ids)
-
-    assert List.first(cities).id == List.first(ids)
-    assert List.last(cities).id == List.last(ids)
+    assert cities_ids == found_cities_ids
   end
 end
